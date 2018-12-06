d1d313e40253ea159f377854d8af7a438bd6733a
hyunseok4475 <33482120+hyunseok4475@users.noreply.github.com>
2018-12-07 02:08:41 +0900

    Create AI_Project_Lung.py

---
+++ AI_Project_Lung.py
@@ -0,0 +1,26 @@
+import cv2
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+import pandas as pd
+
+#하이퍼파라미터 설정
+
+#정상 폐 이미지 해상도 통일 및 파일 이름 변경
+
+#폐렴 폐 이미지 해상도 통일 및 파일 이름 변경
+
+#암 폐 이미지 해상도 통일 및 파일 이름 변경
+
+#정상 폐 훈련 데이터 읽어오기
+
+#폐렴 폐 훈련 데이터 읽어오기
+
+#암 폐 훈련데이터 읽어오기
+
+#테스트 데이터 읽어오기
+
+#테스트
+
+#정확도 출력
