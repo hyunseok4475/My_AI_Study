dd15107184921f35cf7b74d9f2a43456d2f13b15
hyunseok4475 <33482120+hyunseok4475@users.noreply.github.com>
2018-12-07 16:52:09 +0900

    Update AI_Project_Lung.py

--- AI_Project_Lung.py
+++ AI_Project_Lung.py
@@ -5,22 +5,36 @@ import matplotlib.pyplot as plt
 import scipy as sp
 import pandas as pd
 
-#하이퍼파라미터 설정
+epoch = 30
+wide = 500
+height = 500
 
 #정상 폐 이미지 해상도 통일 및 파일 이름 변경
+Y = tf.placeholder(tf.float32, [None, 3])
 
-#폐렴 폐 이미지 해상도 통일 및 파일 이름 변경
+L1 = tf.layers.conv2d(X, 2048, [3, 3])
+L1 = tf.layers.max_pooling2d(L1, [2, 2], [2, 2])
+L1 = tf.layers.dropout(L1, 0.7, is_training)
 
-#암 폐 이미지 해상도 통일 및 파일 이름 변경
+L2 = tf.layers.conv2d(L1, 1024, [3, 3])
+L2 = tf.layers.max_pooling2d(L2, [2, 2], [2, 2])
+L2 = tf.layers.dropout(L2, 0.7, is_training)
 
-#정상 폐 훈련 데이터 읽어오기
+L3 = tf.contrib.layers.flatten(L2)
+L3 = tf.layers.dense(L3, 256, activation = tf.nn.relu)
+L3 = tf.layers.dropout(L3, 0.5, is_training)
 
-#폐렴 폐 훈련 데이터 읽어오기
+model = tf.layers.dense(L3, 10, activation = None)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits = model, labels=Y))
+optimizer = tf.train.RMSPropOptimizer(0.001, 0.8).minimize(cost)
 
-#암 폐 훈련데이터 읽어오기
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
 
-#테스트 데이터 읽어오기
+batch_size = 100
 
-#테스트
-
-#정확도 출력
+for i in range(epoch):
+    total_cost = 0
+    
+    for j in range():
