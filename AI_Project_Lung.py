122955ac7690eae0354f907cf9eb799c7d90e920
hyunseok4475 <33482120+hyunseok4475@users.noreply.github.com>
2018-12-08 14:28:00 +0900

    Update AI_Project_Lung.py

--- AI_Project_Lung.py
+++ AI_Project_Lung.py
@@ -5,26 +5,37 @@ import matplotlib.pyplot as plt
 import scipy as sp
 import pandas as pd
 
-epoch = 30
+training_epoch = 30
 wide = 500
 height = 500
 
 #정상 폐 이미지 해상도 통일 및 파일 이름 변경
+
+
+#컨볼루젼 뉴럴넷 구성
 Y = tf.placeholder(tf.float32, [None, 3])
 
-L1 = tf.layers.conv2d(X, 2048, [3, 3])
+L1 = tf.layers.conv2d(X, 1024, [3, 3])
 L1 = tf.layers.max_pooling2d(L1, [2, 2], [2, 2])
 L1 = tf.layers.dropout(L1, 0.7, is_training)
 
-L2 = tf.layers.conv2d(L1, 1024, [3, 3])
+L2 = tf.layers.conv2d(L1, 2048, [3, 3])
 L2 = tf.layers.max_pooling2d(L2, [2, 2], [2, 2])
 L2 = tf.layers.dropout(L2, 0.7, is_training)
 
-L3 = tf.contrib.layers.flatten(L2)
-L3 = tf.layers.dense(L3, 256, activation = tf.nn.relu)
-L3 = tf.layers.dropout(L3, 0.5, is_training)
+L3 = tf.layers.conv2d(L2, 4096, [3, 3])
+L3 = tf.layers.max_pooling2d(L3, [2, 2], [2, 2])
+L3 = tf.layers.dropout(L3, 0.7, is_training)
 
-model = tf.layers.dense(L3, 10, activation = None)
+L4 = tf.contrib.layers.flatten(L3)
+L4 = tf.layers.dense(L4, 2048, activation = tf.nn.relu)
+L4 = tf.layers.dropout(L4, 0.5, is_training)
+
+L5 = tf.contrib.layers.flatten(L4)
+L5 = tf.layers.dense(L5, 256, activation = tf.nn.relu)
+L5 = tf.layers.dropout(L5, 0.5, is_training)
+
+model = tf.layers.dense(L5, 3, activation = None)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits = model, labels=Y))
 optimizer = tf.train.RMSPropOptimizer(0.001, 0.8).minimize(cost)
 
@@ -34,7 +45,7 @@ sess.run(init)
 
 batch_size = 100
 
-for i in range(epoch):
-    total_cost = 0
-    
+for epoch in range(training_epoch):
+    avg_cost_list = np.zeros(len(models))
+    total_batch = int()
     for j in range():
